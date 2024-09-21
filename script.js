81eccb37de54b859565b824cd14101b9198cb3bd
Lucas Santana <m.ellolucas258@gmail.com>
2024-09-21 18:12:51 -0300

    Troca de swiper pelo Slider

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ var swiper = new Swiper(".swiper", {
     },
     keyboard: true,
   });
+  
+  /*---------JS - Slider---------*/ 
+
+  let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+},9000)
+
+function nextImage(){
+    count++;
+    if(count>6){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
